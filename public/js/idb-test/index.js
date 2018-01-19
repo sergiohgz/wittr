a5f382f019a9a4dee7c9b2a995848747b931d26b
Sergio Herrera Guzmán <sergio.herrera.guzman@tecsisa.com>
2018-01-19 10:05:03 +0100

    L4Q4: More IDB

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,90 +1,114 @@
-import idb from 'idb';
+import idb from "idb";
 
-var dbPromise = idb.open('test-db', 3, function(upgradeDb) {
-  switch(upgradeDb.oldVersion) {
+var dbPromise = idb.open("test-db", 4, function(upgradeDb) {
+  switch (upgradeDb.oldVersion) {
     case 0:
-      var keyValStore = upgradeDb.createObjectStore('keyval');
+      var keyValStore = upgradeDb.createObjectStore("keyval");
       keyValStore.put("world", "hello");
     case 1:
-      upgradeDb.createObjectStore('people', { keyPath: 'name' });
+      upgradeDb.createObjectStore("people", { keyPath: "name" });
     case 2:
-      var peopleStore = upgradeDb.transaction.objectStore('people');
-      peopleStore.createIndex('animal', 'favoriteAnimal');
+      var peopleStore = upgradeDb.transaction.objectStore("people");
+      peopleStore.createIndex("animal", "favoriteAnimal");
+    case 3:
+      var peopleStore = upgradeDb.transaction.objectStore("people");
+      peopleStore.createIndex("age", "age");
   }
   // TODO: create an index on 'people' named 'age', ordered by 'age'
 });
 
 // read "hello" in "keyval"
-dbPromise.then(function(db) {
-  var tx = db.transaction('keyval');
-  var keyValStore = tx.objectStore('keyval');
-  return keyValStore.get('hello');
-}).then(function(val) {
-  console.log('The value of "hello" is:', val);
-});
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("keyval");
+    var keyValStore = tx.objectStore("keyval");
+    return keyValStore.get("hello");
+  })
+  .then(function(val) {
+    console.log('The value of "hello" is:', val);
+  });
 
 // set "foo" to be "bar" in "keyval"
-dbPromise.then(function(db) {
-  var tx = db.transaction('keyval', 'readwrite');
-  var keyValStore = tx.objectStore('keyval');
-  keyValStore.put('bar', 'foo');
-  return tx.complete;
-}).then(function() {
-  console.log('Added foo:bar to keyval');
-});
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("keyval", "readwrite");
+    var keyValStore = tx.objectStore("keyval");
+    keyValStore.put("bar", "foo");
+    return tx.complete;
+  })
+  .then(function() {
+    console.log("Added foo:bar to keyval");
+  });
 
-dbPromise.then(function(db) {
-  var tx = db.transaction('keyval', 'readwrite');
-  var keyValStore = tx.objectStore('keyval');
-  keyValStore.put('cat', 'favoriteAnimal');
-  return tx.complete;
-}).then(function() {
-  console.log('Added favoriteAnimal:cat to keyval');
-});
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("keyval", "readwrite");
+    var keyValStore = tx.objectStore("keyval");
+    keyValStore.put("cat", "favoriteAnimal");
+    return tx.complete;
+  })
+  .then(function() {
+    console.log("Added favoriteAnimal:cat to keyval");
+  });
 
 // add people to "people"
-dbPromise.then(function(db) {
-  var tx = db.transaction('people', 'readwrite');
-  var peopleStore = tx.objectStore('people');
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("people", "readwrite");
+    var peopleStore = tx.objectStore("people");
 
-  peopleStore.put({
-    name: 'Sam Munoz',
-    age: 25,
-    favoriteAnimal: 'dog'
+    peopleStore.put({
+      name: "Sam Munoz",
+      age: 25,
+      favoriteAnimal: "dog"
+    });
+
+    peopleStore.put({
+      name: "Susan Keller",
+      age: 34,
+      favoriteAnimal: "cat"
+    });
+
+    peopleStore.put({
+      name: "Lillie Wolfe",
+      age: 28,
+      favoriteAnimal: "dog"
+    });
+
+    peopleStore.put({
+      name: "Marc Stone",
+      age: 39,
+      favoriteAnimal: "cat"
+    });
+
+    return tx.complete;
+  })
+  .then(function() {
+    console.log("People added");
   });
-
-  peopleStore.put({
-    name: 'Susan Keller',
-    age: 34,
-    favoriteAnimal: 'cat'
-  });
-
-  peopleStore.put({
-    name: 'Lillie Wolfe',
-    age: 28,
-    favoriteAnimal: 'dog'
-  });
-
-  peopleStore.put({
-    name: 'Marc Stone',
-    age: 39,
-    favoriteAnimal: 'cat'
-  });
-
-  return tx.complete;
-}).then(function() {
-  console.log('People added');
-});
 
 // list all cat people
-dbPromise.then(function(db) {
-  var tx = db.transaction('people');
-  var peopleStore = tx.objectStore('people');
-  var animalIndex = peopleStore.index('animal');
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("people");
+    var peopleStore = tx.objectStore("people");
+    var animalIndex = peopleStore.index("animal");
 
-  return animalIndex.getAll('cat');
-}).then(function(people) {
-  console.log('Cat people:', people);
-});
+    return animalIndex.getAll("cat");
+  })
+  .then(function(people) {
+    console.log("Cat people:", people);
+  });
 
 // TODO: console.log all people ordered by age
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("people");
+    var peopleStore = tx.objectStore("people");
+    var ageIndex = peopleStore.index("age");
+
+    return ageIndex.getAll();
+  })
+  .then(function(people) {
+    console.log("People sorted by age: ", people);
+  });
