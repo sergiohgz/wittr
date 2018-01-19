023eefdacebaa1aa667912094c8d4d72b396f32a
Sergio Herrera Guzmán <sergio.herrera.guzman@tecsisa.com>
2018-01-19 09:53:27 +0100

    L4Q3: Getting Started with IDB

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,31 +1,43 @@
-import idb from 'idb';
+import idb from "idb";
 
-var dbPromise = idb.open('test-db', 1, function(upgradeDb) {
-  var keyValStore = upgradeDb.createObjectStore('keyval');
+var dbPromise = idb.open("test-db", 1, function(upgradeDb) {
+  var keyValStore = upgradeDb.createObjectStore("keyval");
   keyValStore.put("world", "hello");
 });
 
 // read "hello" in "keyval"
-dbPromise.then(function(db) {
-  var tx = db.transaction('keyval');
-  var keyValStore = tx.objectStore('keyval');
-  return keyValStore.get('hello');
-}).then(function(val) {
-  console.log('The value of "hello" is:', val);
-});
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("keyval");
+    var keyValStore = tx.objectStore("keyval");
+    return keyValStore.get("hello");
+  })
+  .then(function(val) {
+    console.log('The value of "hello" is:', val);
+  });
 
 // set "foo" to be "bar" in "keyval"
-dbPromise.then(function(db) {
-  var tx = db.transaction('keyval', 'readwrite');
-  var keyValStore = tx.objectStore('keyval');
-  keyValStore.put('bar', 'foo');
-  return tx.complete;
-}).then(function() {
-  console.log('Added foo:bar to keyval');
-});
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("keyval", "readwrite");
+    var keyValStore = tx.objectStore("keyval");
+    keyValStore.put("bar", "foo");
+    return tx.complete;
+  })
+  .then(function() {
+    console.log("Added foo:bar to keyval");
+  });
 
-dbPromise.then(function(db) {
-  // TODO: in the keyval store, set
-  // "favoriteAnimal" to your favourite animal
-  // eg "cat" or "dog"
-});
+dbPromise
+  .then(function(db) {
+    // TODO: in the keyval store, set
+    // "favoriteAnimal" to your favourite animal
+    // eg "cat" or "dog"
+    var tx = db.transaction("keyval", "readwrite");
+    var keyValStore = tx.objectStore("keyval");
+    keyValStore.put("cat", "favoriteAnimal");
+    return tx.complete;
+  })
+  .then(function() {
+    console.log("Added favoriteAnimal:cat to keyval");
+  });
